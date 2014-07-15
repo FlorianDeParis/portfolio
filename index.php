62bbea8b303b372ba73de51f3305d6661b9523e0
FlorianDeParis <florian.tournay@hotmail.fr>
2014-07-15 20:40:47 +0200

    premier commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf8">
+		<link rel="stylesheet" type="text/css" href="./css/style.css">
+		<title>Florian Tournay</title>
+	</head>
+	<body>
+		<div class="corps">
+			<div class="photo_corps">
+				<img src="./img/myself.jpg">
+			</div>
+			<div class="titre_corps">
+				<font class="entete">Florian Tournay</font>
+			</div>
+		</div>
+	</body>
+</html>
