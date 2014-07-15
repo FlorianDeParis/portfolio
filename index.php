0a942e35654d11937bc9c0a526f89d2eede2aaa2
FlorianDeParis <florian.tournay@hotmail.fr>
2014-07-16 00:42:24 +0200

    Creation structure

--- index.php
+++ index.php
@@ -3,16 +3,36 @@
 	<head>
 		<meta charset="utf8">
 		<link rel="stylesheet" type="text/css" href="./css/style.css">
+		<link rel="stylesheet" type="text/css" href="./css/flaticon.css"> 
+		<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+		<script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/1.12.1/TweenMax.min.js"></script>
+		<script type="text/javascript" src="./js/app.js"></script>
 		<title>Florian Tournay</title>
 	</head>
 	<body>
+		<!--CADRE DE PRESENTATION-->
 		<div class="corps">
-			<div class="photo_corps">
-				<img src="./img/myself.jpg">
+			<div class="presentation">
+				<div class="photo_corps">
+					<img src="./img/myself.jpg">
+				</div>
+				<div class="titre_corps">
+					<div class="entete">Florian Tournay</div>
+					<div class="bio">Développeur Web / Graphiste Amateur<br>
+						Actuellement étudiant en troisième année de bachelor développement web au sein de l'ESGI</div>
+				</div>
 			</div>
-			<div class="titre_corps">
-				<font class="entete">Florian Tournay</font>
+			<div class="arrow">
+				<i class="flaticon-arrow128">
+					<span class="popup">Je vous en prie, cliquez!</span>
+				</i>
 			</div>
 		</div>
+
+		<!--CONTENEUR D'INFOS-->
+		<div class="conteneur">
+				TEST
+		</div>
+
 	</body>
 </html>
