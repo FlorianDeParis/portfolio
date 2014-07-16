3e792a1c08ec52d1b48bf96ea7dc0ec31903d016
FlorianDeParis <florian.tournay@hotmail.fr>
2014-07-16 21:36:33 +0200

    ...

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
 		<meta charset="utf8">
 		<link rel="stylesheet" type="text/css" href="./css/style.css">
 		<link rel="stylesheet" type="text/css" href="./css/flaticon.css"> 
-		<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-		<script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/1.12.1/TweenMax.min.js"></script>
+		<script type="text/javascript" src="./js/jquery-2.1.1.min.js"></script>
+		<script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/gsap/1.12.1/TweenMax.min.js"></script>
 		<script type="text/javascript" src="./js/app.js"></script>
 		<title>Florian Tournay</title>
 	</head>
@@ -19,7 +19,7 @@
 				<div class="titre_corps">
 					<div class="entete">Florian Tournay</div>
 					<div class="bio">Développeur Web / Graphiste Amateur<br>
-						Actuellement étudiant en troisième année de bachelor développement web au sein de l'ESGI</div>
+						Etudiant en alternance, passionné des technologies du web, de design ainsi que du high-tech.</div>
 				</div>
 			</div>
 			<div class="arrow">
@@ -31,7 +31,47 @@
 
 		<!--CONTENEUR D'INFOS-->
 		<div class="conteneur">
-				TEST
+			<div class="parcours">
+				<font class="titre_cursus">Cursus Scolaire</font>
+				<ul class="cursus">
+					<li>
+						<span class="annee rotate">2013-2014</span>
+						<div class="etude">
+							<font class="brevet">L3 / Bachelor développement web</font><br>
+							<font class="etablissement">ESGI - Ecole Supérieure de Génie Informatique</font><br>
+							<font class="lieu">Paris 12 - Paris</font>
+						</div>
+						<div class="logo_etablissement">
+							<img src="./img/Logo-ESGI.jpg">
+						</div>
+					</li>
+					<li>
+						<span class="annee rotate">2013</span>
+						<div class="etude">
+							<font class="brevet">BTS IRIS - Informatique des Réseaux de l'Industrie et des Services techniques</font><br>
+							<font class="etablissement">Lycée Polyvalent Dorian</font><br>
+							<font class="lieu">Paris 11 - Paris</font>
+						</div>
+						<div class="logo_etablissement">
+							<img src="./img/Dorian.png">
+						</div>
+					</li>
+					<li>
+						<span class="annee rotate">2011</span>
+						<div class="etude">
+							<font class="brevet">Baccalauréat STI - Sciences Techniques de l'Industrie</font><br>
+							<font class="etablissement">Lycée Polyvalent Louis Armand</font><br>
+							<font class="lieu">Nogent-Sur-Marne - Val de Marne</font>
+						</div>
+						<div class="logo_etablissement">
+							<img src="./img/louis_armand.png">
+						</div>
+					</li>
+				</ul>
+			</div>
+			<div class="competences">
+				COMPETENCES
+			</div>
 		</div>
 
 	</body>
--- js/app.js
+++ js/app.js
@@ -1,4 +1,19 @@
-(function($){
+jQuery().ready(function(){
 
+	var fleche = $(".flaticon-arrow128");
 
-})(jQuery);
+	$(".flaticon-arrow128").mouseover(function(e){
+		fleche.animate({
+			bottom: "-=10"
+		});
+			
+	});
+
+	$(".flaticon-arrow128").mouseleave(function(e){
+		fleche.animate({
+			bottom: "+=10"
+		});
+		
+	});
+
+});
