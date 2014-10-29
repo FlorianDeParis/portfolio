470baed05f11efade798b75a7b5740e7d0b3570d
FlorianDeParis <florian.tournay@hotmail.fr>
2014-10-29 11:36:01 +0100

    ...

--- index.php
+++ index.php
@@ -1,17 +1,26 @@
 <!DOCTYPE html>
-<html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr">
 	<head>
-		<meta charset="utf8">
+		<meta charset="UTF-8">
 		<link rel="stylesheet" type="text/css" href="./css/style.css">
-		<link rel="stylesheet" type="text/css" href="./css/flaticon.css"> 
+		<link rel="stylesheet" type="text/css" href="./css/flaticon.css">
+		<link rel="stylesheet" type="text/css" href="jqueryui/development-bundle/themes/smoothness/jquery.ui.all.css"/> 
 		<script type="text/javascript" src="./js/jquery-2.1.1.min.js"></script>
-		<script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/gsap/1.12.1/TweenMax.min.js"></script>
+		<!--<script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/gsap/1.12.1/TweenMax.min.js"></script>-->
 		<script type="text/javascript" src="./js/app.js"></script>
+		<!--[if IE]>
+      		<link rel="stylesheet" type="text/css" href="./css/ie.css" />
+      	<![endif]-->
 		<title>Florian Tournay</title>
 	</head>
-	<body>
+	<body onload="loading()">
+		<div id="chargement">
+				<span class="loader loader-quart"></span>
+				Chargement...
+		</div>
+
 		<!--CADRE DE PRESENTATION-->
-		<div class="corps">
+		<div class="corps" id="site">
 			<div class="presentation">
 				<div class="photo_corps">
 					<img src="./img/myself.jpg">
@@ -19,7 +28,7 @@
 				<div class="titre_corps">
 					<div class="entete">Florian Tournay</div>
 					<div class="bio">Développeur Web / Graphiste Amateur<br>
-						Etudiant en alternance, passionné des technologies du web, de design ainsi que du high-tech.</div>
+						Etudiant en alternance, passionné de design, des technologies du web ainsi que du high-tech.</div>
 				</div>
 			</div>
 			<div class="arrow">
@@ -29,48 +38,376 @@
 			</div>
 		</div>
 
-		<!--CONTENEUR D'INFOS-->
+		<!-- MENU CATEGORIES -->
+		<div class="menu">
+			<div class="menu_cv">
+				Curriculum vitae
+			</div>
+			<div class="menu_projet">
+				Projets
+			</div>
+			<div class="menu_contact">
+				Me contacter
+			</div>
+		</div>
+
+		<!--CONTENEUR CURRICULUM VITAE-->
 		<div class="conteneur">
+
+			<!--COLONNE GAUCHE -->
 			<div class="parcours">
-				<font class="titre_cursus">Cursus Scolaire</font>
-				<ul class="cursus">
+
+				<!--CURSUS SCOLAIRE-->
+				<div class="parcours_scolaire">
+					<div class="titre_cursus">&nbsp;&nbsp;Cursus Scolaire</div>
+					<ul class="liste_des_cursus">
+						<li>
+							<span class="annee rotate">2014-2016</span>
+							<div class="etude">
+								<font class="brevet">Master - Ingénierie du Web et E-Business (espéré)</font><br>
+								<font class="etablissement">ESGI - Ecole Supérieure de Génie Informatique</font><br>
+								<font class="lieu">Paris 12 - Paris</font>
+							</div>
+							<div class="logo_etablissement">
+								<img src="./img/Logo-ESGI.png">
+							</div>
+						</li>
+						<li>
+							<span class="annee rotate">2014</span>
+							<div class="etude">
+								<font class="brevet">Bachelor (équivalent Licence) - Développement web</font><br>
+								<font class="etablissement">ESGI - Ecole Supérieure de Génie Informatique</font><br>
+								<font class="lieu">Paris 12 - Paris</font>
+							</div>
+							<div class="logo_etablissement">
+								<img src="./img/Logo-ESGI.png">
+							</div>
+						</li>
+						<li>
+							<span class="annee rotate">2013</span>
+							<div class="etude">
+								<font class="brevet">BTS IRIS - Informatique des Réseaux de l'Industrie et des Services techniques</font><br>
+								<font class="etablissement">Lycée Polyvalent Dorian</font><br>
+								<font class="lieu">Paris 11 - Paris</font>
+							</div>
+							<div class="logo_etablissement">
+								<img src="./img/Dorian.png">
+							</div>
+						</li>
+						<li>
+							<span class="annee rotate">2011</span>
+							<div class="etude">
+								<font class="brevet">Baccalauréat STI - Sciences Techniques de l'Industrie <font style="font-family: Origin-ExtraBold, Arial; font-size: 10px;">- (Spé. Génie Electronique Numérique)</font></font><br>
+								<font class="etablissement">Lycée Polyvalent Louis Armand</font><br>
+								<font class="lieu">Nogent-Sur-Marne - Val de Marne</font>
+							</div>
+							<div class="logo_etablissement">
+								<img src="./img/louis_armand.png">
+							</div>
+						</li>
+					</ul>
+				</div>
+
+
+
+				<!--EXPERIENCE PROFESIONNELLE-->
+				<div class="parcours_professionnel">
+					<div class="titre_cursus" style="margin-bottom: 15px">&nbsp;&nbsp;Expérience professionnelle</div>
+					<ul class="liste_des_cursus">
+						<li>
+							<span class="annee rotate">2014-2016</span>
+							<div class="etude">
+								<font class="brevet"><i>En recherche d'entreprise ...</i></font><br>
+								<font class="etablissement"></font><br>
+								<font class="lieu"></font>
+							</div>
+							<div class="logo_etablissement">
+								<img src="./img/turn-arrow.png">
+							</div>
+						</li>
+						<li>
+							<span class="annee rotate">2013-2014</span>
+							<div class="etude">
+								<font class="brevet">Analyste informatique / développeur web front-end</font><br>
+								<font class="etablissement">HSBC - Courbevoie (La Défense)</font><br>
+								<font class="lieu">Contrat professionnel de 11 mois</font>
+							</div>
+							<div class="logo_etablissement">
+								<img src="./img/HSBC.png">
+							</div>
+						</li>
+						<li>
+							<span class="annee rotate">2012</span>
+							<div class="etude">
+								<font class="brevet">Stagiaire développeur / testeur en Jython</font><br>
+								<font class="etablissement">Axway - Puteaux</font><br>
+								<font class="lieu">Stage de découverte du Jython</font>
+							</div>
+							<div class="logo_etablissement">
+								<img src="./img/Axway.png">
+							</div>
+						</li>
+						<li>
+							<span class="annee rotate">2007</span>
+							<div class="etude">
+								<font class="brevet">Stagiaire développeur / testeur en Pascal et MySQL</font><br>
+								<font class="etablissement">KDI - Aubervilliers</font><br>
+								<font class="lieu">Stage de découverte du Pascal et de MySQL</font>
+							</div>
+							<div class="logo_etablissement">
+								<img src="./img/logo-kdi.png">
+							</div>
+						</li>
+					</ul>
+				</div>
+
+
+				<!--PASSIONS / INTERETS -->	
+				<div class="liste_interets">
+					<div class="titre_cursus" style="margin-bottom: 15px">&nbsp;&nbsp;Passions / intérêts</div>
+					<ul class="interets">
+						<li>
+							<div class="etude">
+								<font class="brevet">Sport:</font><br>
+								<font class="etablissement">Badminton, VTT, Sport en salle</font>
+							</div>
+						</li>
+						<li>
+							<div class="etude">
+								<font class="brevet">Passions:</font><br>
+								<font class="etablissement">Musique (DJing) , Internet, Webdesign, High-Tech, Smartphones, Android, Linux</font>
+							</div>
+						</li>
+						<li>
+							<div class="etude">
+								<font class="brevet">Activités:</font><br>
+								<font class="etablissement">Composition et mixage de musiques (DJing),<br>
+									design d'un site d'actualités numériques, <br>
+									maintenance et réparation de smartphones et PCs,<br>
+									jeux en ligne,<br>
+									promenades,<br>
+									création d'un réseau multimédia domestique.
+									</font>
+							</div>
+						</li>
+					</ul>
+				</div>
+
+
+			</div>
+
+			<!--COMPETENCES TECHNIQUES-->
+			<div class="competences">
+				<div class="titre_competences">&nbsp;&nbsp;Compétences</div>
+				<ul class="competences_liste">
 					<li>
-						<span class="annee rotate">2013-2014</span>
-						<div class="etude">
-							<font class="brevet">L3 / Bachelor - Développement web</font><br>
-							<font class="etablissement">ESGI - Ecole Supérieure de Génie Informatique</font><br>
-							<font class="lieu">Paris 12 - Paris</font>
-						</div>
-						<div class="logo_etablissement">
-							<img src="./img/Logo-ESGI.jpg">
-						</div>
+						<div class="competences_cat_label">Développement</div>
+						<ul class="competences_cat competences_liste_dev">
+							<li class="liste_matiere">
+								<div class="matiere">HTML5 / XHTML</div>
+								<div class="classement">
+									<img src="./img/html-tag-active-green.png"><!----><img src="./img/html-tag-active-green.png"><!--
+									--><img src="./img/html-tag-active-green.png"><!----><img src="./img/html-tag-active-green.png"><!--
+									--><img src="./img/html-tag-active-green.png"><!----><img src="./img/html-tag-active-green.png"><!--
+									--><img src="./img/html-tag.png">
+								</div>
+							</li>
+							<li class="liste_matiere">
+								<div class="matiere">CSS3</div>
+								<div class="classement">
+									<img src="./img/html-tag-active-green.png"><!----><img src="./img/html-tag-active-green.png"><!--
+									--><img src="./img/html-tag-active-green.png"><!----><img src="./img/html-tag-active-green.png"><!--
+									--><img src="./img/html-tag-active-green.png"><!----><img src="./img/html-tag-active-green.png"><!--
+									--><img src="./img/html-tag.png">
+								</div>
+							</li>
+							<li class="liste_matiere">
+								<div class="matiere">JS / jQuery / Ajax</div>
+								<div class="classement">
+									<img src="./img/html-tag-active-green.png"><!----><img src="./img/html-tag-active-green.png"><!--
+									--><img src="./img/html-tag-active-green.png"><!----><img src="./img/html-tag-active-green.png"><!--
+									--><img src="./img/html-tag.png"><!----><img src="./img/html-tag.png"><!--
+									--><img src="./img/html-tag.png">
+								</div>
+							</li>
+							<li class="liste_matiere">
+								<div class="matiere">PHP objet</div>
+								<div class="classement">
+									<img src="./img/html-tag-active-green.png"><!----><img src="./img/html-tag-active-green.png"><!--
+									--><img src="./img/html-tag-active-green.png"><!----><img src="./img/html-tag.png"><!--
+									--><img src="./img/html-tag.png"><!----><img src="./img/html-tag.png"><!--
+									--><img src="./img/html-tag.png">
+								</div>
+							</li>
+							<li class="liste_matiere">
+								<div class="matiere">CMS (WordPress)</div>
+								<div class="classement">
+									<img src="./img/html-tag-active-green.png"><!----><img src="./img/html-tag-active-green.png"><!--
+									--><img src="./img/html-tag-active-green.png"><!----><img src="./img/html-tag.png"><!--
+									--><img src="./img/html-tag.png"><!----><img src="./img/html-tag.png"><!--
+									--><img src="./img/html-tag.png">
+								</div>
+							</li>
+							<li class="liste_matiere">
+								<div class="matiere">Python</div>
+								<div class="classement">
+									<img src="./img/html-tag-active-green.png"><!----><img src="./img/html-tag-active-green.png"><!--
+									--><img src="./img/html-tag-active-green.png"><!----><img src="./img/html-tag.png"><!--
+									--><img src="./img/html-tag.png"><!----><img src="./img/html-tag.png"><!--
+									--><img src="./img/html-tag.png">
+								</div>
+							</li>
+							<li class="liste_matiere">
+								<div class="matiere">Java</div>
+								<div class="classement">
+									<img src="./img/html-tag-active-green.png"><!----><img src="./img/html-tag-active-green.png"><!--
+									--><img src="./img/html-tag.png"><!----><img src="./img/html-tag.png"><!--
+									--><img src="./img/html-tag.png"><!----><img src="./img/html-tag.png"><!--
+									--><img src="./img/html-tag.png">
+								</div>
+							</li>
+							<li class="liste_matiere">
+								<div class="matiere">C / C++</div>
+								<div class="classement">
+									<img src="./img/html-tag-active-green.png"><!----><img src="./img/html-tag-active-green.png"><!--
+									--><img src="./img/html-tag.png"><!----><img src="./img/html-tag.png"><!--
+									--><img src="./img/html-tag.png"><!----><img src="./img/html-tag.png"><!--
+									--><img src="./img/html-tag.png">
+								</div>
+							</li>
+							<li class="liste_matiere">
+								<div class="matiere">Shell Script</div>
+								<div class="classement">
+									<img src="./img/html-tag-active-green.png"><!----><img src="./img/html-tag-active-green.png"><!--
+									--><img src="./img/html-tag-active-green.png"><!----><img src="./img/html-tag-active-green.png"><!--
+									--><img src="./img/html-tag-active-green.png"><!----><img src="./img/html-tag.png"><!--
+									--><img src="./img/html-tag.png">
+								</div>
+							</li>
+						</ul>
 					</li>
 					<li>
-						<span class="annee rotate">2013</span>
-						<div class="etude">
-							<font class="brevet">BTS IRIS - Informatique des Réseaux de l'Industrie et des Services techniques</font><br>
-							<font class="etablissement">Lycée Polyvalent Dorian</font><br>
-							<font class="lieu">Paris 11 - Paris</font>
-						</div>
-						<div class="logo_etablissement">
-							<img src="./img/Dorian.png">
-						</div>
+						<div class="competences_cat_label">Design</div>
+						<ul class="competences_cat competences_liste_design">
+							<li class="liste_matiere">
+								<div class="matiere">Responsive</div>
+								<div class="classement">
+									<img src="./img/html-tag-active-yellow.png"><!----><img src="./img/html-tag-active-yellow.png"><!--
+									--><img src="./img/html-tag-active-yellow.png"><!----><img src="./img/html-tag-active-yellow.png"><!--
+									--><img src="./img/html-tag.png"><!----><img src="./img/html-tag.png"><!--
+									--><img src="./img/html-tag.png">
+								</div>
+							</li>
+							<li class="liste_matiere">
+								<div class="matiere">Photoshop</div>
+								<div class="classement">
+									<img src="./img/photoshop-active-yellow.png"><!----><img src="./img/photoshop-active-yellow.png"><!--
+									--><img src="./img/photoshop-active-yellow.png"><!----><img src="./img/photoshop-active-yellow.png"><!--
+									--><img src="./img/photoshop.png"><!----><img src="./img/photoshop.png"><!--
+									--><img src="./img/photoshop.png">
+								</div>
+							</li>
+							<li class="liste_matiere">
+								<div class="matiere">Illustrator</div>
+								<div class="classement">
+									<img src="./img/illustrator-active-yellow.png"><!----><img src="./img/illustrator-active-yellow.png"><!--
+									--><img src="./img/illustrator-active-yellow.png"><!----><img src="./img/illustrator.png"><!--
+									--><img src="./img/illustrator.png"><!----><img src="./img/illustrator.png"><!--
+									--><img src="./img/illustrator.png">
+								</div>
+							</li>
+						</ul>
 					</li>
 					<li>
-						<span class="annee rotate">2011</span>
-						<div class="etude">
-							<font class="brevet">Baccalauréat STI - Sciences Techniques de l'Industrie</font><br>
-							<font class="etablissement">Lycée Polyvalent Louis Armand</font><br>
-							<font class="lieu">Nogent-Sur-Marne - Val de Marne</font>
-						</div>
-						<div class="logo_etablissement">
-							<img src="./img/louis_armand.png">
-						</div>
+						<div class="competences_cat_label">Systèmes</div>
+						<ul class="competences_cat competences_liste_sys">
+							<li class="liste_matiere">
+								<div class="matiere">Windows XP/Vista/7/8/8.1</div>
+								<div class="classement">
+									<img src="./img/windows11-active-red.png"><!----><img src="./img/windows11-active-red.png"><!--
+									--><img src="./img/windows11-active-red.png"><!----><img src="./img/windows11-active-red.png"><!--
+									--><img src="./img/windows11-active-red.png"><!----><img src="./img/windows11.png"><!--
+									--><img src="./img/windows11.png">
+								</div>
+							</li>
+							<li class="liste_matiere">
+								<div class="matiere">Linux (Debian, ... )</div>
+								<div class="classement">
+									<img src="./img/linux1-active-red.png"><!----><img src="./img/linux1-active-red.png"><!--
+									--><img src="./img/linux1-active-red.png"><!----><img src="./img/linux1-active-red.png"><!--
+									--><img src="./img/linux1.png"><!----><img src="./img/linux1.png"><!--
+									--><img src="./img/linux1.png">
+								</div>
+							</li>
+						</ul>
+					</li>
+					<li>
+						<div class="competences_cat_label">Langues</div>
+						<ul class="competences_cat competences_liste_langues">
+							<li class="liste_matiere">
+								<div class="matiere">Anglais</div>
+								<div class="classement">
+									<img src="./img/united2-active-green.png"><!----><img src="./img/united2-active-green.png"><!--
+									--><img src="./img/united2-active-green.png"><!----><img src="./img/united2-active-green.png"><!--
+									--><img src="./img/united2.png"><!----><img src="./img/united2.png"><!--
+									--><img src="./img/united2.png">
+								</div>
+							</li>
+							<li class="liste_matiere">
+								<div class="matiere">Espagnol</div>
+								<div class="classement">
+									<img src="./img/spain-active-green.png"><!----><img src="./img/spain-active-green.png"><!--
+									--><img src="./img/spain.png"><!----><img src="./img/spain.png"><!--
+									--><img src="./img/spain.png"><!----><img src="./img/spain.png"><!--
+									--><img src="./img/spain.png">
+								</div>
+							</li>
+						</ul>
+					</li>
+					<li class="legende">
+						<div class="legende_label">Légende</div>
+						<ul>
+							<li><div class="legende_points">1 point:</div>Niveau débutant</li>
+							<li><div class="legende_points">2 points:</div>Niveau débutant avancé</li>
+							<li><div class="legende_points">3 points:</div>Niveau moyen</li>
+							<li><div class="legende_points">4 points:</div>Niveau acceptable</li>
+							<li><div class="legende_points">5 points:</div>Niveau bon</li>
+							<li><div class="legende_points">6 points:</div>Niveau avancé</li>
+							<li><div class="legende_points">7 points:</div>Niveau très bon</li>
+						</ul>
 					</li>
 				</ul>
 			</div>
-			<div class="competences">
-				COMPETENCES
+		</div>
+
+		<!--CONTENEUR DE PROJETS-->
+		<div class="conteneur2">
+			<div class="projet_sites">
+				<div class="titre_projet_sites">&nbsp;&nbsp;Sites internet</div>
+				<div class="contenu_projet_sites">
+					<fieldset>
+						<legend style="font-family: Exo-Light, Arial">Antica.fr</legend>
+						<a href="http://www.antica.fr"><img src="./img/antica.png"></a>
+						<span style="font-family: Exo-Light, Arial">Debogage et design du site</span>
+					</fieldset>
+				</div>
+			</div>
+			<div class="projet_design">
+				<div class="titre_projet_reas">&nbsp;&nbsp;Réalisations graphiques</div>
+				<div class="contenu_projet_reas">
+					<fieldset>
+						<legend style="font-family: Exo-Light, Arial">Exemple</legend>
+						<img src="./img/antica.png">
+						<span style="font-family: Exo-Light, Arial">Lorem Ipsum</span>
+					</fieldset>
+				</div>
+			</div>
+		</div>
+
+		<div class="conteneur3">
+			<div>&nbsp;&nbsp;Me contacter</div>
+			<div>
+				Lorem Ipsum
 			</div>
 		</div>
 
--- js/app.js
+++ js/app.js
@@ -1,19 +1,75 @@
+function loading(){
+  document.getElementById('chargement').style.display='none';
+  document.getElementById('site').style.visibility='visible';
+}
+
 jQuery().ready(function(){
 
-	var fleche = $(".flaticon-arrow128");
+	// On cache la zone de texte
+   	jQuery('.conteneur').hide();  // Zone "conteneur" caché au démarrage
+   	jQuery('.conteneur2').hide(); // Zone "conteneur2" caché au démarrage
+    jQuery('.conteneur3').hide(); // Zone "conteneur3" caché au démarrage
+   	jQuery('.menu').hide();       // Zone "menu" caché au démarrage
 
-	$(".flaticon-arrow128").mouseover(function(e){
-		fleche.animate({
-			bottom: "-=10"
-		});
-			
-	});
+   	var boolean_cv = false;			  // Etat du conteneur CV qui est fermé de base
+   	var boolean_projet = false;		// Etat du conteneur Projets qui est fermé de base
+   	var boolean_open = true;		  // Etat du contenu qui est fermé de base
 
-	$(".flaticon-arrow128").mouseleave(function(e){
-		fleche.animate({
-			bottom: "+=10"
-		});
-		
-	});
+   	// toggle() lorsque le lien avec la classe .flaticon-arrow128 est cliqué
+   	jQuery('.flaticon-arrow128').click(function()
+  	{
+  		jQuery('.menu').slideToggle(1000); // Animation toggle du menu
+    	jQuery('.conteneur').slideToggle(2000); // Animation toggle du contenu
+    	boolean_open = false;
+    	return false;
+   	});
+
+   	$('.menu_cv').click(function()
+  	{
+  		if(boolean_open == false){ // Si le menu est déroulé
+	  		$(".conteneur").effect("drop", {mode:"show"}, 1000);  // Animation de déroulement de conteneur
+        $(".conteneur2").effect("drop", {mode:"hide"}, 1000); // Animation d'enroulement de conteneur
+        /*$(".conteneur").show("drop", {direction:"up"}, 1000);
+	  		$(".conteneur2").hide("drop", {direction:"up"}, 1000);*/
+  		}
+   	});
+
+   	$('.menu_projet').click(function()
+  	{
+  		if(boolean_open == false){ // Si le menu est déroulé
+	  		$(".conteneur2").effect("drop", {mode:"show"}, 1000); // Animation de déroulement de conteneur2
+	  		$(".conteneur").effect("drop", {mode:"hide"}, 1000);  // Animation d'enroulement de conteneur
+        /*$(".conteneur").hide("drop", {direction:"up"}, 1000);
+        $(".conteneur2").show("drop", {direction:"up"}, 1000);*/
+	  	}
+   	});
+    
+    // =======> ANIMATION TEMPORAIRE <=======
+    /*jQuery('.menu_cv').click(function()
+    {
+      if(boolean_open == false){
+        jQuery('.conteneur').show();
+        jQuery('.conteneur2').hide();
+        jQuery('.conteneur3').hide();
+      }
+    });
+
+    jQuery('.menu_projet').click(function()
+    {
+      if(boolean_open == false){
+        jQuery('.conteneur2').show();
+        jQuery('.conteneur').hide();
+        jQuery('.conteneur3').hide();
+      }
+    });
+
+    jQuery('.menu_contact').click(function()
+    {
+      if(boolean_open == false){
+        jQuery('.conteneur3').show();
+        jQuery('.conteneur2').hide();
+        jQuery('.conteneur').hide();
+      }
+    });*/
 
 });
