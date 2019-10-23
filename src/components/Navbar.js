675f82102c7180d9b8076357aa5d9211dbde748a
Florian <florian.tournay@hotmail.fr>
2019-10-23 15:52:05 +0200

    Fix navbar / change senitive case Class name

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import '../assets/styles/nav.scss';
+
+export default class NavBar extends React.Component{
+  render(){
+    return (
+      <nav>
+        <Link to="/"><span id="logo"></span></Link>
+        <ul className="container">
+          <li><Link to="/accueil">ACCUEIL</Link></li>
+          <li><Link to="/experiences">EXPERIENCES</Link></li>
+          <li><Link to="/diplomes">DIPLOMES</Link></li>
+          <li><Link to="/projets">PROJETS</Link></li>
+          <li><Link to="/apropos">A PROPOS</Link></li>
+        </ul>
+      </nav>
+    );
+  }
+}
